f25f1ab3dd6fe3b445f2f01d1a81173df93e55fb
arthurcadore <arthurbarcella.ifsc@gmail.com>
2025-02-20 18:25:29 +0000

    updated entrypoint.sh to use the correct path for the config file

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -17,12 +17,6 @@ echo "###############################################"
 make tools/clean
 make tools/compile V=s
 make tools/install V=s
-make package/utils/jsonfilter/clean
-make package/utils/jsonfilter/compile V=s
-make package/feeds/simetbox/simetbox-openwrt-simet-lmapd/clean
-make package/feeds/simetbox/simetbox-openwrt-simet-lmapd/compile V=s
-make package/feeds/simetbox/simetbox-openwrt-simet-ma/clean
-make package/feeds/simetbox/simetbox-openwrt-simet-ma/compile V=s
 make package/system/procd/clean
 make package/system/procd/compile V=s
 make package/system/rpcd/clean
