d0e823a47d3389638c5c9e2b9d99b6b2ec1710c9
arthurcadore <arthurbarcella.ifsc@gmail.com>
2025-03-12 09:43:46 +0000

    :sparkles: added cp for flock

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -36,6 +36,8 @@ echo "###############################################"
 
 cp -r includes/* staging_dir/target-aarch64-openwrt-linux-musl_musl/root-mediatek/
 
+cp /host/staging_dir/target-aarch64-openwrt-linux-musl_musl/root-mediatek/usr/bin/flock /host/staging_dir/target-aarch64-openwrt-linux-musl_musl/root-mediatek/usr/bin/util-linux-flock
+
 echo "###############################################"
 echo "## Compressing Output File...                ##"
 echo "###############################################"
